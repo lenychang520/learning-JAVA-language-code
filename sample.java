0d8e392bc36b4f90122b2f718419853857f15e7a
Leny Chang <141756738+lenychang520@users.noreply.github.com>
2026-01-14 21:27:41 +0800

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,107 @@
+//package com.leny.text;
+//
+//public class sample {
+//    public static void main(String[] args) {
+//        double wechatpay = 0;
+//        double alipay = 10;
+//        double paycard = 20;
+//        double sum = wechatpay + alipay + paycard;
+//        System.out.println(sum);
+//        System.out.println(wechatpay + 10 - 2);
+//    }
+//}
+
+
+//package com.leny.text;
+//
+//public class sample {
+//    public static void main(String[] args) {
+//        double HP = 1223.3, bonus = 1.2;
+//        int defense = 80, attack = 220;
+//        int damage1 = attack;
+//        HP -= damage1 - defense;
+//        System.out.println("我方造成的普攻伤害为" + damage1 + "敌人剩余血量为" + HP);
+//        double damage2 = bonus * attack;
+//        HP -= damage2 - defense;
+//        System.out.println("我方造成的技能伤害为" + damage2 + "敌人剩余血量为" + HP);
+//    }
+//}
+
+
+//package com.leny.text;
+//
+//public class sample {
+//    public static void main(String[] args) {
+//        long a = 1000L;
+//        float b = 50.0f;
+//        System.out.println(a + " " + b);
+//    }
+//}
+
+
+//package com.leny.text;
+//
+//import javax.xml.transform.Source;
+//import java.sql.SQLOutput;
+//
+//public class sample {
+//    public static void main() {
+//        double height = 1.78;
+//        double weight = 77.70;
+//        double BMI = weight / (height * height);
+//        System.out.println(BMI);
+//        double max_weight = height * height * 23.9;
+//        System.out.println(max_weight);
+//    }
+//}
+
+
+//package com.leny.text;
+//
+//import java.util.Scanner;
+//
+//public class sample {
+//    public static void main() {
+//        Scanner sc = new Scanner(System.in);//告知scanner在哪（sc），并告知scanner从键盘输入（in）
+//        int num = sc.nextInt();//用告知的地址（sc）输入整数
+//        double num2 = sc.nextDouble();//输入double
+//        String s = sc.next();//输入字符串
+//        System.out.println(num + " " + num2 + " " + s);
+//    }
+//}
+
+
+//package com.leny.text;
+//
+//import java.util.Scanner;
+//
+//public class sample {
+//    public static void main(String[] args) {
+//        int num1 = 0, num2 = 0;
+//        Scanner sc = new Scanner(System.in);
+//        num1 = sc.nextInt();
+//        num2 = sc.nextInt();
+//        int sum = num1 + num2;
+//        System.out.println(sum);
+//    }
+//}
+
+
+//package com.leny.text;
+//
+//import java.util.Scanner;
+//
+//public class sample {
+//    public static void main(String[] args) {
+//        double height = 0, weight = 0;
+//        Scanner a = new Scanner(System.in);
+//        System.out.println("请输入你的身高");
+//        height = a.nextDouble();
+//        System.out.println("请输入你的体重");
+//        weight = a.nextDouble();
+//        double BMI = weight / (height * height);
+//        System.out.println("你的BMI指数为" + BMI);
+//    }
+//}
+
+
